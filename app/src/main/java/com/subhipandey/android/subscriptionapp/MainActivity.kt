ca8c92279074ab06242d5d07789369189adf2f2d
Subhi Pandey <subhi2012cpi@gmail.com>
2020-05-29 16:56:19 +0530

    Added RecyclerView to Main Activity and Impleted Update&Delete Functions

--- app/src/main/java/com/subhipandey/android/subscriptionapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/subscriptionapp/MainActivity.kt
@@ -3,9 +3,13 @@ package com.subhipandey.android.subscriptionapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.subhipandey.android.subscriptionapp.database.Subscriber
 import com.subhipandey.android.subscriptionapp.database.SubscriberDatabase
 import com.subhipandey.android.subscriptionapp.database.SubscriberRepository
 import com.subhipandey.android.subscriptionapp.databinding.ActivityMainBinding
@@ -17,19 +21,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val dao = SubscriberDatabase.getInstance(application).subscriberDAO
         val repository = SubscriberRepository(dao)
         val factory = SubscriberViewModelFactory(repository)
-        subscriberViewModel = ViewModelProvider(this,factory).get(SubscriberViewModel::class.java)
+        subscriberViewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
         binding.myViewModel = subscriberViewModel
         binding.lifecycleOwner = this
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
         displaySubscribersList()
     }
     private fun displaySubscribersList(){
         subscriberViewModel.subscribers.observe(this, Observer {
             Log.i("MYTAG",it.toString())
+            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(it,{selectedItem:Subscriber->listItemClicked(selectedItem)})
 
         })
+    }
+
+    private fun listItemClicked(subscriber: Subscriber){
+        Toast.makeText(this, "selected name is ${subscriber.name}",Toast.LENGTH_LONG).show()
+        subscriberViewModel.initUpdateAndDelete(subscriber)
     }
 }
